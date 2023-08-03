9726f7595900912648a88d58b3933672ebc25e82
chenjingyu <2458006366@qq.com>
2023-08-03 11:49:53 +0800

    Update：添加视频模式的Mediapipe人脸mesh测试代码；

--- examples/TestFaceDetection.cc
+++ examples/TestFaceDetection.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: chenjingyu
  * @Date: 2023-07-29 17:05:18
- * @LastEditTime: 2023-08-01 18:02:47
+ * @LastEditTime: 2023-08-03 11:48:01
  * @Description: Test face detection
  * @FilePath: \Mediapipe-MNN\examples\TestFaceDetection.cc
  */
@@ -13,35 +13,12 @@
 
 
 using namespace mirror;
+#define USE_VIDEO 
 
 int main(int argc, char *argv[]) {
   const char *image_file = "../data/images/face_tongue.jpg";
-  cv::Mat image = cv::imread(image_file);
-  if (image.empty()) {
-    std::cout << "failed load image." << std::endl;
-    return -1;
-  }
-  RotateType type = RotateType::CLOCKWISE_ROTATE_0;
-  if (type == CLOCKWISE_ROTATE_90) {
-    cv::transpose(image, image);
-  } else if (type == CLOCKWISE_ROTATE_180) {
-    cv::flip(image, image, -1);
-  }
-  if (type == CLOCKWISE_ROTATE_270) {
-    cv::transpose(image, image);
-    cv::flip(image, image, 1);
-  }
-  ImageHead in;
-  in.data = image.data;
-  in.height = image.rows;
-  in.width = image.cols;
-  in.width_step = image.step[0];
-  in.pixel_format = PixelFormat::BGR;
-
-  const char *face_model_file =
-      "../data/models/face_detection_short_range_fp16.mnn";
-  const char *face_landmark_model_file =
-      "../data/models/face_landmarks_detector_fp16.mnn";
+  const char *face_model_file =  "../data/models/face_detection_short_range_fp16.mnn";
+  const char *face_landmark_model_file = "../data/models/face_landmarks_detector_fp16.mnn";
   FaceDetector detector;
   FaceLandmarkDetector landmarker;
   if (!detector.LoadModel(face_model_file) ||
@@ -49,32 +26,80 @@ int main(int argc, char *argv[]) {
     std::cout << "Failed load model." << std::endl;
     return -1;
   }
-  detector.setSourceFormat(in.pixel_format);
-  landmarker.setSourceFormat(in.pixel_format);
-  // detector.setUseFull();
+  detector.setSourceFormat(PixelFormat::BGR);
+  landmarker.setSourceFormat(PixelFormat::BGR);
+
+  cv::Mat frame;
+#ifdef USE_VIDEO
+  cv::VideoWriter writer;
+  cv::VideoCapture cam(0);
+  if (!cam.isOpened()) {
+    printf("Failed open camera.\n");
+    return -2;
+  }
+  while (cv::waitKey(1) != 'q') {
+    cam >> frame;
+    if (!writer.isOpened())
+      writer.open("mediapipe.mp4", CV_FOURCC('X', 'V', 'I', 'D'), 25,
+                  cv::Size(frame.cols, frame.rows));
+#else
+    frame = cv::imread(image_file, 1);
+#endif
+    cv::Mat frame_src = frame.clone();
+    if (frame.empty()) {
+      printf("Empty frame.\n");
+      return -3;
+    }
+
+  RotateType type = RotateType::CLOCKWISE_ROTATE_0;
+  if (type == CLOCKWISE_ROTATE_90) {
+    cv::transpose(frame, frame);
+  } else if (type == CLOCKWISE_ROTATE_180) {
+    cv::flip(frame, frame, -1);
+  }
+  if (type == CLOCKWISE_ROTATE_270) {
+    cv::transpose(frame, frame);
+    cv::flip(frame, frame, 1);
+  }
+  ImageHead in;
+  in.data = frame.data;
+  in.height = frame.rows;
+  in.width = frame.cols;
+  in.width_step = frame.step[0];
+  in.pixel_format = PixelFormat::BGR;
+
 
   std::vector<ObjectInfo> objects;
   detector.Detect(in, type, objects);
   landmarker.Detect(in, type, objects);
   for (const auto &object : objects) {
-    cv::rectangle(image, cv::Point2f(object.rect.left, object.rect.top),
+    cv::rectangle(frame_src, cv::Point2f(object.rect.left, object.rect.top),
                   cv::Point2f(object.rect.right, object.rect.bottom),
                   cv::Scalar(255, 0, 255), 2);
     for (int i = 0; i < object.landmarks.size(); ++i) {
       cv::Point pt = cv::Point((int)object.landmarks[i].x, (int)object.landmarks[i].y);
-      cv::circle(image, pt, 2, cv::Scalar(255, 255, 0));
-      cv::putText(image, std::to_string(i), pt, 1, 1.0, cv::Scalar(255, 0, 255));
+      cv::circle(frame_src, pt, 2, cv::Scalar(255, 255, 0));
+      cv::putText(frame_src, std::to_string(i), pt, 1, 1.0, cv::Scalar(255, 0, 255));
     }
     for (int i = 0; i < object.landmarks3d.size(); ++i) {
       cv::Point pt = cv::Point((int)object.landmarks3d[i].x, (int)object.landmarks3d[i].y);
-      cv::circle(image, pt, 2, cv::Scalar(255, 255, 0));
+      cv::circle(frame_src, pt, 2, cv::Scalar(255, 255, 0));
     }
-    cv::putText(image, std::to_string(object.score),
+    cv::putText(frame_src, std::to_string(object.score),
                 cv::Point2f(object.rect.left, object.rect.top + 20), 1, 1.0,
                 cv::Scalar(0, 255, 255));
   }
-  cv::imshow("result", image);
+#ifdef USE_VIDEO
+    cv::imshow("frame", frame_src);
+    int key = cv::waitKey(2);
+    if (key == 27) break;
+    writer << frame_src;
+  }
+  cam.release();
+  writer.release();
+#else
+  cv::imshow("frame", frame_src);
   cv::waitKey(0);
-
+#endif
   return 0;
 }
